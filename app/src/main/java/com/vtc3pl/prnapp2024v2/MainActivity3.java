e6cd2ddd3d30104f750633c7a24ea1ac8cfbce4c
manoj-swatpro2024 <manoj.kale@vtc3pl.co.in>
2024-05-02 00:34:14 +0530

    Added Contract party fetch functionality

--- app/src/main/java/com/vtc3pl/prnapp2024v2/MainActivity3.java
+++ app/src/main/java/com/vtc3pl/prnapp2024v2/MainActivity3.java
@@ -1,10 +1,18 @@
 package com.vtc3pl.prnapp2024v2;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +20,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    private TextView showUserNameTextViewActivityThree;
     private EditText editTextFromDateActivityThree, editTextToDateActivityThree;
     private Calendar fromCalendar, toCalendar;
     private DatePickerDialog.OnDateSetListener fromDateSetListener, toDateSetListener;
+    private String username = "", depo = "", year = "";
+    private AutoCompleteTextView contractPartyAutoCompleteTextView;
+    private List<String> contractPartiesList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +55,26 @@ public class MainActivity3 extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
 
+        showUserNameTextViewActivityThree = findViewById(R.id.showUserNameTextViewActivityThree);
         editTextFromDateActivityThree = findViewById(R.id.editTextFromDateActivityThree);
         editTextToDateActivityThree = findViewById(R.id.editTextToDateActivityThree);
+        contractPartyAutoCompleteTextView = findViewById(R.id.contractPartyAutoCompleteTextView);
 
         fromCalendar = Calendar.getInstance();
         toCalendar = Calendar.getInstance();
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            username = intent.getStringExtra("username");
+            depo = intent.getStringExtra("depo");
+            year = intent.getStringExtra("year");
+
+            // Set the fetched username to the TextView
+            if (username != null) {
+                String usernameText = getString(R.string.user_name_prefix, username);
+                showUserNameTextViewActivityThree.setText(usernameText);
+            }
+        }
 
         fromDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -51,6 +95,38 @@ public class MainActivity3 extends AppCompatActivity {
                 updateToDate();
             }
         };
+
+        contractPartiesList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, contractPartiesList);
+        contractPartyAutoCompleteTextView.setAdapter(adapter);
+
+        contractPartyAutoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedContractParty = (String) parent.getItemAtPosition(position);
+                String custName = selectedContractParty.split(" : ")[1];
+                contractPartyAutoCompleteTextView.setText(custName);
+            }
+        });
+
+        contractPartyAutoCompleteTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = s.toString().trim();
+                if (!input.isEmpty()) {
+                    fetchContractParties(input);
+                }
+                contractPartyAutoCompleteTextView.showDropDown();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -75,5 +151,61 @@ public class MainActivity3 extends AppCompatActivity {
     private void updateToDate() {
         String dateFormat = "yyyy/MM/dd";
         editTextToDateActivityThree.setText(android.text.format.DateFormat.format(dateFormat, toCalendar));
+    }
+
+    private void fetchContractParties(String input) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("depo", depo) // Assuming depo is defined in your class
+                .add("contractParty", input)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://vtc3pl.com/fetch_contract_party_prn_app.php")
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String responseData = response.body() != null ? response.body().string() : null;
+                    Log.d("Response 1 : ", responseData);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            handleResponse(responseData);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void handleResponse(String responseData) {
+        try {
+            JSONArray jsonArray = new JSONArray(responseData);
+            contractPartiesList.clear();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String contractParty = jsonObject.getString("CustCode") + " : " + jsonObject.getString("CustName") + " : " + jsonObject.getString("IndType");
+                Log.d("Response 2 ", "contract party : " + contractParty);
+                contractPartiesList.add(contractParty);
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.notifyDataSetChanged();
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
